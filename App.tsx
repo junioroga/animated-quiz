d44660c25761576e1611abaccbd0b63e0f3d7b63
Vilson Oga Júnior <junioroga@outlook.com>
2024-11-06 10:08:48 -0300

    initial commit

---
+++ App.tsx
@@ -0,0 +1,20 @@
+import { Roboto_400Regular, Roboto_700Bold, useFonts } from '@expo-google-fonts/roboto'
+import { StatusBar } from 'react-native'
+
+import { Loading } from './src/components/Loading'
+import { Routes } from './src/routes'
+
+export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
+
+  if (!fontsLoaded) {
+    return <Loading />
+  }
+
+  return (
+    <>
+      <StatusBar barStyle="light-content" backgroundColor="transparent" translucent />
+      <Routes />
+    </>
+  )
+}
---
+++ src/components/Option/index.tsx
@@ -0,0 +1,16 @@
+import { Text, TouchableOpacity, TouchableOpacityProps } from 'react-native'
+
+import { styles } from './styles'
+
+type Props = TouchableOpacityProps & {
+  checked: boolean
+  title: string
+}
+
+export function Option({ checked, title, ...rest }: Props) {
+  return (
+    <TouchableOpacity style={[styles.container, checked && styles.checked]} {...rest}>
+      <Text style={styles.title}>{title}</Text>
+    </TouchableOpacity>
+  )
+}
